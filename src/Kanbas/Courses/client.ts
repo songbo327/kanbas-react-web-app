ae6bc9b8f67c0b20ca71330bda9e8d78047f0220
songbo327 <songbog327@gmail.com>
2024-06-14 01:44:19 +0800

    fix a5 _id bug

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -19,6 +19,6 @@ export const deleteCourse = async (id: string) => {
 };
 
 export const updateCourse = async (course: any) => {
-    const response = await axios.put(`${COURSES_API}/${course.id}`, course);
+    const response = await axios.put(`${COURSES_API}/${course._id}`, course);
     return response.data;
 };
